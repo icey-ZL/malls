edc98af34a9f06b812723a01fd040cc45fc6032e
icey-ZL <843124961@qq.com>
2021-02-19 20:50:47 +0800

    swiper in home

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,9 +2,9 @@
   <div class="home">
     <nav-bar>
       <div slot="center">购物街</div>
-      <div slot="center">购物街</div>
-      <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banner="banner"></home-swiper>
+
     <h2>首页</h2>
   </div>
 
@@ -13,18 +13,28 @@
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
-  import { getHomeMultidata } from "network/home";
-
+  import { getHomeMultidata } from 'network/home';
+  import HomeSwiper from './childrenCompnent/HomeSwiper'
   export default {
     name: "Home",
     components:{
-        NavBar
+      NavBar,
+      HomeSwiper,
+    },
+    data(){
+      return{
+        banner:[],
+        recommend:[]
+      }
     },
     created(){
       //请求多个数据
       getHomeMultidata()
       .then(res=>{
-          console.log(res);
+        //保存结果
+        //created的外层作用域是组件实例
+        this.banner = res.data.banner.list
+        this.recommend = res.data.recommend.list
         }).catch(err=>err)
     }
   }
---
+++ src/views/home/childrenCompnent/HomeSwiper.vue
@@ -0,0 +1,31 @@
+<template>
+  <swiper>
+    <swiper-item v-for="(item,index) in banner" :key="index" >
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import {Swiper,SwiperItem} from 'components/common/swiper/index'
+    export default {
+      name: "HomeSwiper",
+      props:{
+        banner:{
+          type:Array,
+          default() {
+            return [];
+          }
+        }
+      },
+      components:{
+        Swiper,
+        SwiperItem
+      },
+    }
+</script>
+<style scoped>
+
+</style>
